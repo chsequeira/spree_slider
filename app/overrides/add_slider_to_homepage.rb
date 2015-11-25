7b5fdad77e942d4bb9c8993904b6da808ae30407
Christian <ch.sequeira@chweb.pt>
2015-11-25 23:33:28 +0000

    Deface adjustments

---
+++ app/overrides/add_slider_to_homepage.rb
@@ -0,0 +1,7 @@
+Deface::Override.new(
+  virtual_path: 'spree/shared/_header',
+  name: 'add_slider_to_homepage',
+  insert_after: '#spree-header',
+  partial: 'spree/shared/homepage_slider',
+  original: '92c159ac9d2aec631cbdbd556c82b361923d63c5' 
+)
