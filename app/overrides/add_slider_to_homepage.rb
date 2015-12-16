677e0f8235f1cc0533a3f9ef6549b8f239bb9fa7
Christian <ch.sequeira@chweb.pt>
2015-12-16 14:29:22 +0000

    Change slider location from after header to replace banner

--- app/overrides/add_slider_to_homepage.rb
+++ app/overrides/add_slider_to_homepage.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
-  virtual_path: 'spree/shared/_header',
+  virtual_path: 'spree/shared/_banner',
   name: 'add_slider_to_homepage',
-  insert_after: '#spree-header',
+  replace_contents: '#spree-banner',
   partial: 'spree/shared/homepage_slider',
   original: '92c159ac9d2aec631cbdbd556c82b361923d63c5' 
 )
